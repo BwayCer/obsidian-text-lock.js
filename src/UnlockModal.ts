bbff386ba17bf537ba7fd2d2d3272c0ebaf30c69
BwayCer <bwaycer@gmail.com>
2025-07-06 21:54:52 +0800

    feat: modify 格式化 UnlockModal.ts

--- src/UnlockModal.ts
+++ src/UnlockModal.ts
@@ -1,87 +1,87 @@
-import { App, Modal, Setting, Notice } from 'npm:obsidian';
+import { App, Modal, Notice, Setting } from "npm:obsidian";
 
 interface UnlockModalResult {
-    selectedKeyName: string;
-    password: string;
+  selectedKeyName: string;
+  password: string;
 }
 
 export class UnlockModal extends Modal {
-    private resolve: (value: UnlockModalResult) => void;
-    private reject: (reason?: any) => void;
-    private submitted = false;
+  private resolve: (value: UnlockModalResult) => void;
+  private reject: (reason?: any) => void;
+  private submitted = false;
 
-    private keyNames: string[];
-    private selectedKeyName: string;
-    private password: string;
+  private keyNames: string[];
+  private selectedKeyName: string;
+  private password: string;
 
-    constructor(app: App, keyNames: string[]) {
-        super(app);
-        this.keyNames = keyNames;
-        this.selectedKeyName = keyNames.length > 0 ? keyNames[0] : '';
-        this.password = '';
-    };
+  constructor(app: App, keyNames: string[]) {
+    super(app);
+    this.keyNames = keyNames;
+    this.selectedKeyName = keyNames.length > 0 ? keyNames[0] : "";
+    this.password = "";
+  }
 
-    onOpen() {
-        const { contentEl } = this;
+  onOpen() {
+    const { contentEl } = this;
 
-        this.titleEl.setText('Unlock Note');
+    this.titleEl.setText("Unlock Note");
 
-        // Key Dropdown
-        new Setting(contentEl)
-            .setName('Select Key')
-            .addDropdown(dropdown => {
-                this.keyNames.forEach(name => {
-                    dropdown.addOption(name, name);
-                });
-                dropdown.setValue(this.selectedKeyName);
-                dropdown.onChange(value => {
-                    this.selectedKeyName = value;
-                });
-            });
-
-        // Password Input
-        new Setting(contentEl)
-            .setName('Password')
-            .addText(text => {
-                text.setPlaceholder('Enter password')
-                    .setValue(this.password)
-                    .onChange(value => {
-                        this.password = value;
-                    })
-                    .inputEl.type = 'password'; // Make it a password field
-            });
-
-        // Submit Button
-        new Setting(contentEl)
-            .addButton(button => {
-                button.setButtonText('Unlock')
-                    .setCta()
-                    .onClick(() => {
-                        this.submitted = true;
-                        this.close();
-                    });
-            });
-    }
-
-    onClose() {
-        const { contentEl } = this;
-        contentEl.empty();
-
-        if (this.submitted) {
-            this.resolve({
-                selectedKeyName: this.selectedKeyName,
-                password: this.password,
-            });
-        } else {
-            this.reject('Modal closed without submission');
-        }
-    }
-
-    openAndAwaitResult(): Promise<UnlockModalResult> {
-        return new Promise((resolve, reject) => {
-            this.resolve = resolve;
-            this.reject = reject;
-            this.open();
+    // Key Dropdown
+    new Setting(contentEl)
+      .setName("Select Key")
+      .addDropdown((dropdown) => {
+        this.keyNames.forEach((name) => {
+          dropdown.addOption(name, name);
         });
+        dropdown.setValue(this.selectedKeyName);
+        dropdown.onChange((value) => {
+          this.selectedKeyName = value;
+        });
+      });
+
+    // Password Input
+    new Setting(contentEl)
+      .setName("Password")
+      .addText((text) => {
+        text.setPlaceholder("Enter password")
+          .setValue(this.password)
+          .onChange((value) => {
+            this.password = value;
+          })
+          .inputEl.type = "password"; // Make it a password field
+      });
+
+    // Submit Button
+    new Setting(contentEl)
+      .addButton((button) => {
+        button.setButtonText("Unlock")
+          .setCta()
+          .onClick(() => {
+            this.submitted = true;
+            this.close();
+          });
+      });
+  }
+
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+
+    if (this.submitted) {
+      this.resolve({
+        selectedKeyName: this.selectedKeyName,
+        password: this.password,
+      });
+    } else {
+      this.reject("Modal closed without submission");
     }
+  }
+
+  openAndAwaitResult(): Promise<UnlockModalResult> {
+    return new Promise((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+      this.open();
+    });
+  }
 }
