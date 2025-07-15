7ababf4e6547772cf66494d990d5b06ce84d20b4
BwayCer <bwaycer@gmail.com>
2025-07-16 00:15:03 +0800

    feat: modify MySimple 改從個別文件中拉取設定資料

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,11 @@
 import {
   CryptoConfigOptional,
+} from "https://raw.githubusercontent.com/BwayCer/tendon.js/refs/tags/v1/src/browser/crypto/MySimple.ts";
+import { transformBase64 } from "https://raw.githubusercontent.com/BwayCer/tendon.js/refs/tags/v1/src/browser/crypto/simple/transformConfig.ts";
+import {
   derivePbkdf2Sha256e6,
   encryptAesGcm256,
-  transformBase64,
-} from "https://raw.githubusercontent.com/BwayCer/tendon.js/refs/tags/v1/src/browser/crypto/MySimple.ts";
+} from "https://raw.githubusercontent.com/BwayCer/tendon.js/refs/tags/v1/src/browser/crypto/simple/v202507.ts";
 
 export type { Result } from "https://raw.githubusercontent.com/BwayCer/tendon.js/refs/tags/v1/src/types/strongLang.ts";
 
